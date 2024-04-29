9363fcf17e87e67a255277196253410f24239b9b
VadimMor <rich246135@gmail.com>
2024-04-29 18:45:48 +0500

    Добавление подключения и проверка к бд

---
+++ src/main/java/biletka/main/entity/User.java
@@ -0,0 +1,22 @@
+package biletka.main.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "role")
+    private String role;
+    @Column(name = "status")
+    private Boolean status;
+}
